b70934d2e597f501ba39da01a7f9901919b71f7e
duncan.njoroge <kienjdu@gmail.com>
2021-11-15 19:56:40 +0300

    updated employeedata

--- employeedata.py
+++ employeedata.py
@@ -1,7 +1,8 @@
-from _typeshed import SupportsReadline
 
 
 class Employee:
+    num_of_emps = 0
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -9,17 +10,34 @@ class Employee:
         self.pay  = pay
         self.email = first + '.' + last + '@company.com'
 
+
+        Employee.num_of_emps += 1
+        
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+# this raise is hidden in this method. 
+# But it can also be instanciated at the top. from the class. 
 
 
 emp_1= Employee('duncan', 'njoroge', 50000)
 emp_2= Employee('hellen', 'njoroge', 60000)
 
+print(emp_1.__dict__)
 
-#print(emp_1.email)
-#print(emp_2.email)
+emp_1.raise_amount = 1.05
 
 
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
 
-#print(emp_1.fullname())
+"""
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_2.pay)
+"""
+
+
